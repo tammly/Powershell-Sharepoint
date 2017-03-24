fae53d190d40c0456abe9050d754ae3c824f4118
Tam <tam.ly.it@gmail.com>
2017-03-23 22:48:36 -0600

    Updated comments
    Adding comments to help explain sections of code.

--- BulkDLSP.ps1
+++ BulkDLSP.ps1
@@ -1,4 +1,5 @@
-﻿# Working script.  Bulk files download files from Sharepoint using encrypted creds.
+﻿# Part of this script was created from different sources with a lot of additional custom code.
+ # Working script.  Bulk files download files from Sharepoint using encrypted creds.
 # The script will prompt for a password on the first run.  It will create password files that can be used for subsequent runs.
 
 # Set the variables 
@@ -38,20 +39,24 @@ $webclient = New-Object System.Net.WebClient
 $webclient.Credentials = $credentials
 #$webclient.UseDefaultCredentials = $true
 # the addition at the end of the next line is required if you are not using default credentials.  Use just $source with defaultcreds.
-$webString = $webClient.DownloadString($source + "Forms/AllItems.aspx") 
+$webString = $webClient.DownloadString($source + "Forms/AllItems.aspx")
+# After downloading the entire html page, next step is to split page into lines.
 $lines = [Regex]::Split($webString, "<br>")
+# Create a string from the source that will contain the subfolders we will search for.
 $sub = $source.Split('/')
 $sub = $sub[3..($sub.Length-1)]
 $sub = $sub -join '/'
 
+# this next section will parse through the lines and locate file names for downloading.  Needs improvement.
 foreach ($line in $lines) {
     if ($line.ToUpper().Contains("HREF")) {
-            # File or Folder            
+            # File            
             if (!$line.ToUpper().Contains("[TO PARENT DIRECTORY]")) {
-                # Not Parent Folder entry
+                # Exclude Parent Folder
                 $items =[Regex]::Split($line, """")
                 foreach ($item in $items){
                     if ($item.contains($sub)){ 
+                    # Search for subfolder string of SP site 
                     $item = ($item.split('/'))[-1]                    
                         if ($item -ne "AllItems.aspx"){$webClient.DownloadFile("$source$item", "$destination$item")}
                     
